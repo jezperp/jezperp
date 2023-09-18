4c140823e6b7034ecf4120b9e936dbc17c5d525a
Jesper Pettersson <jesper.pettersson@creo.se>
2023-09-18 21:27:57 +0200

    Added text roation with timeout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,27 @@
 <template>
     <main>
-        <h1>Welcome</h1>
+        <h1 class="text-center" v-html="text" v-for="(text, tkey) in texts" :class="tkey === index ? 'show' : null"></h1>
     </main>
 </template>
-<script setup>
-    
+<script>
+    export default {
+        data() {
+            return {
+                texts: ['Welcome', 'to my', 'portfolio'],
+                index: 0
+            }
+        },
+        methods: {
+            init() {
+                setTimeout(() => {
+                    if(this.index < this.texts.length - 1) this.index++
+                    else this.index = 0
+                    this.init()
+                }, 3000)
+            }
+        },
+        mounted() {
+            this.init()
+        }
+    }
 </script>
