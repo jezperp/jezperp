eab1838e73d0d4689a227fc2b77f2666c3a9805c
Jesper Pettersson <jesper.pettersson@creo.se>
2023-09-20 20:35:54 +0200

    Added header and changed some content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,54 @@
 <template>
-    <main>
-        <h1 class="text-center" v-html="text" v-for="(text, tkey) in texts" :class="tkey === index ? 'show' : null"></h1>
+    <main class="d-flex flex-column">
+        <header :class="showHeader ? 'show' : null">
+            <div class="row align-items-center">
+                <div class="col">
+                    <h3 class="m-0 text-white">Jesper Pettersson</h3>
+                </div>
+                <div class="col-auto ml-auto">
+                    <a  href="#"
+                        @click.prevent=""
+                        class="menu">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </a>
+                </div>
+            </div>
+        </header>
+        <section    id="home"
+                    class="text-center">
+            <div class="pt-5 pb-3">
+                <h1 class="animated"
+                    :class="tkey === index ? 'show' : null"
+                    v-html="text"
+                    v-for="(text, tkey) in texts"
+                    :key="tkey"></h1>
+            </div>
+        </section>
     </main>
 </template>
 <script>
     export default {
         data() {
             return {
-                texts: ['Welcome', 'to my', 'portfolio'],
-                index: 0
+                texts: ['', 'Hello!', 'My name is Jesper', 'This is my resumé page', 'Want to see more?', 'Scroll down to explore!'],
+                index: 0,
+                showHeader: false
             }
         },
         methods: {
             init() {
                 setTimeout(() => {
-                    if(this.index < this.texts.length - 1) this.index++
-                    else this.index = 0
-                    this.init()
-                }, 3000)
+                    if(this.index < this.texts.length - 1) {
+                        this.index++
+                        this.init()
+                    } else {
+                        setTimeout(() => {
+                            this.showHeader = true
+                        }, 2000)
+                    }
+                }, 2500)
             }
         },
         mounted() {
